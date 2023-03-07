6d2db1322dd835ca4b70664c654328126cf64662
MAleemH <aleemhashmi4321@gmail.com>
2023-03-07 20:36:26 +0500

    Added rocketsSlice initalState

--- src/components/rockets/rockets.js
+++ src/components/rockets/rockets.js
@@ -1,6 +1,14 @@
+import './rockets.css';
+import Rocket from './rocket.jpg';
+
 const Rockets = () => (
-  <div className="home-container">
-    <p>hello world</p>
+  <div className="rockets-container">
+    <img src={Rocket} className="rocket-image" alt="Rocket-Image" />
+    <div className="rocket-data">
+      <p className="rocket-title">Falcon 1</p>
+      <p className="rocket-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id orci eu ex malesuada tincidunt ac et dolor. Nunc vel dolor porta, semper felis ut, convallis nibh.</p>
+      <button type="button" className="reservation-button">Reserve Rocket</button>
+    </div>
   </div>
 );
 
