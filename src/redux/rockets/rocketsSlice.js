36e23a9fadecf046d5be2c3a5ebccd1d3b3f967f
MAleemH <aleemhashmi4321@gmail.com>
2023-03-08 11:01:40 +0500

    Fetched data from rockets API

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,12 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  allRockets: [],
-};
+// rockets API
+const rocketsAPI = 'https://api.spacexdata.com/v4/rockets';
+
+// fetching data from API
+
+export const fetchRocketsData = createAsyncThunk('fetchRocketsData', async () => {
+  const response = await fetch(rocketsAPI);
+  const data = await response.json();
+  const allRockets = [];
+  Object.keys(data).forEach((id) => {
+    allRockets.push({
+      id: id,
+      rocket_name: data[id][0].rocket_name,
+      description: data[id][0].description,
+      flickr_images: data[id][0].flickr_images,
+    });
+  });
+  return allRockets;
+});
 
 const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState,
+  initialState: [],
+  extraReducers: builder => {
+    builder
+    .addCase(fetchRocketsData.fulfilled, (state, action) => ({
+      ...state,
+      rocket: [...action.payload],
+    }))
+  }
 });
 
 export default rocketsSlice.reducer;
