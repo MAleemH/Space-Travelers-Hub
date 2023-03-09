c98f402e3fd34ddef154ab5a364754a98677032d
Ermi <manermidem@gmail.com>
2023-03-09 15:15:39 +0300

    Add status functionality

--- src/components/missions/eachMission.js
+++ src/components/missions/eachMission.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import JoinMissionBtn from './joinMissionBtn';
+import './missions.css';
 
-function EachMission({ missionName, missionId, description }) {
+function EachMission({
+  missionName, missionId, description, joinedMission,
+}) {
+  const statusClass = joinedMission ? 'active-member' : 'not-a-member';
+
   return (
     <tr>
       <td className="td mission-title">
@@ -10,13 +16,13 @@ function EachMission({ missionName, missionId, description }) {
       <td className="td">
         <p className="mission-description">{description}</p>
       </td>
-      <td className="td">
-        <span className="status-not-a-member">NOT A MEMBER</span>
+      <td className="td td-with-span">
+        <span className={`${statusClass} status`}>
+          {joinedMission ? 'Active Member' : 'NOT A MEMBER'}
+        </span>
       </td>
-      <td className="td">
-        <button className="join-mission-btn" type="button" id={missionId}>
-          Join Mission
-        </button>
+      <td className="td button-td">
+        <JoinMissionBtn missionId={missionId} joinedMission={joinedMission} />
       </td>
     </tr>
   );
@@ -26,6 +32,7 @@ EachMission.propTypes = {
   missionName: PropTypes.string.isRequired,
   missionId: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  joinedMission: PropTypes.bool.isRequired,
 };
 
 export default EachMission;
